e2c11aa18187d7bf29cf20235a266d2e8b5b0b7a
prantoran <prantoran@gmail.com>
2018-05-28 14:25:51 +0600

    running kafka1 instance with advertised listeners

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
-docker-compose exec kafka kafka-topics --create --topic senz --partitions 2 --replication-factor 1 --if-not-exists --zookeeper 192.168.4.93:2181
+docker-compose exec kafka1 kafka-topics --create --topic senz --partitions 2 --replication-factor 1 --if-not-exists --zookeeper 192.168.4.93:2181
 
-docker-compose exec kafka kafka-topics --create --topic renz --partitions 3 --replication-factor 1 --if-not-exists --zookeeper 192.168.4.93:2181
+docker-compose exec kafka1 kafka-topics --create --topic renz --partitions 3 --replication-factor 1 --if-not-exists --zookeeper 192.168.4.93:2181
 
 docker run --rm ches/kafka kafka-topics.sh --list --zookeeper 192.168.4.93:2181
